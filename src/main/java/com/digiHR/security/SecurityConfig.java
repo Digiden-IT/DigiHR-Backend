c2aa251a9a7b8d7dae781b69c8c70b67949c081f
jahid-digientit <jahid@digidenit.com>
2025-03-24 14:41:28 +0600

    fix( config ): Fixed security related config

--- src/main/java/com/digiHR/security/SecurityConfig.java
+++ src/main/java/com/digiHR/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
     private static final String[]  PUBLIC_API_ENDPOINTS = {
             "/auth/**",
-            "/boilerplate-api-docs/**",
+            "/digihr-api-docs/**",
             "/swagger-ui/**",
             "/v3/api-docs/**"
     };
