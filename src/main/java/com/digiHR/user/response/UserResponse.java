c5b1a0e4e9d2fe709a48eb92477d039246cfb5fa
rahatDigidenit25 <rahat@digidenit.com>
2025-04-12 19:21:53 +0600

    feat(user): modified in user response for get users api

--- src/main/java/com/digiHR/user/response/UserResponse.java
+++ src/main/java/com/digiHR/user/response/UserResponse.java
@@ -1,6 +1,6 @@
 package com.digiHR.user.response;
 
-import com.digiHR.user.Role;
+import com.digiHR.user.*;
 import com.digiHR.user.model.User;
 import lombok.Data;
 
@@ -10,12 +10,31 @@ public class UserResponse {
     private Long id;
     private String name;
     private String email;
+    private String phone_number;
+    private Department department;
     private Role role;
+    private String employeeCode;
+    private EmployeeType employeeType;
+    private String designation;
+    private String dateOfBirth;
+    private Gender gender;
+    private String address;
+    private BloodGroup bloodGroup;
+    private String dateOfJoining;
 
     public UserResponse( User user ) {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+        this.department = user.getDepartment();
         this.role = user.getRole();
+        this.employeeCode = user.getEmployeeCode();
+        this.employeeType = user.getEmployeeType();
+        this.designation = user.getDesignation();
+        this.dateOfBirth = user.getDateOfBirth();
+        this.gender = user.getGender();
+        this.address = user.getAddress();
+        this.bloodGroup = user.getBloodGroup();
+        this.dateOfJoining = user.getDateOfJoining();
     }
 }
